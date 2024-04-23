adf3e652566b88dd07320c9a98e8bc1d7f1883c5
Noorun29 <abdullahnoorun@gmail.com>
2024-04-23 14:56:31 +0200

    register page created

--- iRenew/src/app/app.module.ts
+++ iRenew/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent    // importing our NavBar
+    NavBarComponent,
+    RegisterPageComponent  
   ],
   imports: [
     BrowserModule,
