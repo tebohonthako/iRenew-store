46952cc7e7698d678ce719bc2188ca437ae27d2a
mekachi015 <abielmakoti@gmail.com>
2024-04-23 16:29:58 +0200

    billing-pages

--- iRenew/src/app/app.module.ts
+++ iRenew/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BillingPageComponent } from './pages/billing-page/billing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent    // importing our NavBar
+    NavBarComponent,
+    BillingPageComponent    // importing our NavBar
   ],
   imports: [
     BrowserModule,
