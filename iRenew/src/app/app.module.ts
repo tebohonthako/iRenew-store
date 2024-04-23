de9b2fc77bfe1d17cd449f538c22426c0948241d
Nomfundoluthuli <Nomfundo.luthuli2706@gmail.com>
2024-04-23 14:59:43 +0200

    login component

--- iRenew/src/app/app.module.ts
+++ iRenew/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent    // importing our NavBar
+    NavBarComponent,
+    LoginComponent    
   ],
   imports: [
     BrowserModule,
